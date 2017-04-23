e5595593075c1e7acb22411860dcc4be7a3679ce
imshashwataggarwal <shashwat.aggarwal9@gmail.com>
2017-04-23 19:26:57 +0530

    Added TODO!

--- app/src/main/java/com/example/suche/travelify/Constants.java
+++ app/src/main/java/com/example/suche/travelify/Constants.java
@@ -11,5 +11,6 @@ public class Constants {
     public static final String CUR_UID = "cur_uid";
     public static final String CUR_MAJOR = "cur_major";
     public static final String CUR_MINOR = "cur_minor";
-
+    public static final String DESTINATION_CITY_LAT = "destination_city_lat";
+    public static final String DESTINATION_CITY_LON = "destination_city_lon";
 }
