16a8d42b36de61afb06d89e521399188494b336e
Shashwat Aggarwal <imshashwataggarwal@users.noreply.github.com>
2017-04-27 17:09:21 +0530

    Update Constants.java

--- app/src/main/java/com/example/suche/travelify/Notify/Constants.java
+++ app/src/main/java/com/example/suche/travelify/Notify/Constants.java
@@ -6,4 +6,6 @@ package com.example.suche.travelify.Notify;
 
 public class Constants {
     public static final String IS_BEACON = "is_beacon";
+    public static String UID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+    public static int major = 22537, minor = 18225;
 }
